edf373f4e36ec51a110da7c2fad9fa333adba281
mamudsamiul <samiulmamud1995@gmail.com>
2020-09-24 13:54:20 +0530

    (Samiul Mamud) Added: No play, snake, and ladder portion is added

--- src/com/cg/day3_2/SnakeAndLadder.java
+++ src/com/cg/day3_2/SnakeAndLadder.java
@@ -5,6 +5,10 @@ public class SnakeAndLadder {
 
 	public static void main(String[] args) {
 		
+		final int IS_LADDER=0;
+		final int IS_SNAKE=1;
+		final int NO_PLAY=2;
+		
 		
 		
 		int player_one_position=0;
@@ -16,6 +20,21 @@ public class SnakeAndLadder {
 		System.out.println("Welcome to Snake and Ladder game");
 		
 		System.out.println("Your Number is  : "+ numberCheck);
+		int moveOption=rand.nextInt()%3;
+		
+		switch(moveOption) {
+		case IS_LADDER:
+			player_one_position+=numberCheck;
+			break;
+		case IS_SNAKE:
+			player_one_position-=numberCheck;
+			break;
+		default:
+			
+		
+		}
+		
+		System.out.println("Your Final position is: "+player_one_position);
 		
 
 	}
