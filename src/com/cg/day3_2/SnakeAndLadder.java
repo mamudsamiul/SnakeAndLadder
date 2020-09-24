7e77b948c33bc69bc2f754b28a8b7bd90623580c
mamudsamiul <samiulmamud1995@gmail.com>
2020-09-24 13:48:45 +0530

    (Samiul Mamud) Added: Use case One is added

--- src/com/cg/day3_2/SnakeAndLadder.java
+++ src/com/cg/day3_2/SnakeAndLadder.java
@@ -3,7 +3,11 @@ package com.cg.day3_2;
 public class SnakeAndLadder {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to snake and Ladder Game");
+		
+		int player_one_position=0;
+		
+		System.out.println("Welcome to Snake and Ladder game");
+		
 
 	}
 
